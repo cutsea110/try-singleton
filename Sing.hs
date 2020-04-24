314b1f0bf434fc3393156b16d4a1114b71077ffb
cutsea110 <cutsea110@gmail.com>
2020-04-24 20:28:02 +0900

    define Sing Maybe

--- Sing.hs
+++ Sing.hs
@@ -1,4 +1,23 @@
-{-# LANGUAGE KindSignatures, PolyKinds, TypeFamilies #-}
+{-# LANGUAGE KindSignatures, PolyKinds, TypeFamilies, GADTs, DataKinds, RankNTypes #-}
 module Sing where
 
 data family Sing (a :: k)
+
+data instance Sing (a :: Bool) where
+  STrue  :: Sing 'True
+  SFalse :: Sing 'False
+
+data Nat where
+  Zero :: Nat
+  Succ :: Nat -> Nat
+
+data instance Sing (a :: Nat) where
+  SZero :: Sing 'Zero
+  SSucc :: Sing n -> Sing ('Succ n)
+
+type SNat (a :: Nat)   = Sing a
+type SBool (a :: Bool) = Sing a
+
+data instance Sing (b :: Maybe k) where
+  SNothing :: Sing 'Nothing
+  SJust    :: forall (a :: k). Sing a -> Sing ('Just a)

d1420795254ea06c3f19b379fcd04356d49eb5e0
cutsea110 <cutsea110@gmail.com>
2020-04-24 11:56:05 +0900

    singleton

--- Test.hs
+++ Test.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE GADTs, KindSignatures, DataKinds #-}
+{-# LANGUAGE GADTs, TypeOperators, RankNTypes,
+             KindSignatures, DataKinds, TypeFamilies #-}
 module Test where
 
 data Nat where
@@ -8,3 +9,12 @@ data Nat where
 data Vec :: * -> Nat -> * where
   VNil :: Vec a 'Zero
   VCons :: a -> Vec a n -> Vec a ('Succ n)
+
+type family   (m :: Nat) :< (n :: Nat) :: Bool
+type instance m          :< 'Zero      = 'False
+type instance 'Zero      :< ('Succ n)  = 'True
+type instance ('Succ m)  :< ('Succ n)  = m :< n
+
+data SNat :: Nat -> * where
+  SZero :: SNat 'Zero
+  SSucc :: forall (n :: Nat). SNat n -> SNat ('Succ n)
